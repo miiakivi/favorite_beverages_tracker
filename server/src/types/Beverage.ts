af411f77378846c15f08c89181c26c10e4c4c853
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-02-09 20:00:28 +0200

    refactor: added enum and added type to Beverage type

--- server/src/types/Beverage.ts
+++ server/src/types/Beverage.ts
@@ -1,6 +1,12 @@
 export type Beverage = {
+  type: BeverageType;
   name: string;
   weight: number;
   price: number;
   roast?: number;
+}
+
+export enum BeverageType {
+  Coffee = 0,
+  Tea = 1,
 }

9d48d56512aac7e36ed88d55b3d82836cda0beb4
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-02-09 20:01:18 +0200

    refactor: modified mock database and added correct types and enums to data

--- server/src/modules/inMemoryBeverageModule.ts
+++ server/src/modules/inMemoryBeverageModule.ts
@@ -1,26 +1,29 @@
-import type { Beverage } from "../types/Beverage";
+import { Beverage, BeverageType } from "../types/Beverage";
 
-const coffees: Beverage[] = [
+// Local database with mock data
+const beverages: Beverage[] = [
   {
-    name: "Espresso",
+    type: BeverageType.Coffee,
+    name: "Juhla Mokka",
     weight: 250,
     price: 2.99,
     roast: 5
   },
   {
+    type: BeverageType.Coffee,
     name: "Kulta Katriina",
     weight: 500,
     price: 6.99,
     roast: 1
   },
-];
-const teas: Beverage[] = [
   {
+    type: BeverageType.Tea,
     name: "Green Tea",
     weight: 250,
     price: 4.99,
   },
   {
+    type: BeverageType.Tea,
     name: "Orange Rooibos",
     weight: 200,
     price: 2.99,

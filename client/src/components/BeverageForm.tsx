ec3779baf5401dde134c3bc7868805aeb0e820de
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-02-09 21:41:34 +0200

    feat: added form that adds beverages to app. functionality not done to add beverages

---
+++ client/src/components/BeverageForm.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "../styles/BeverageForm.css";
+
+type CoffeeFormProps = {
+  onSubmit: ( formData: { name: string; weight: number; price: number; roast: number } ) => void;
+}
+
+const BeverageForm: React.FC<CoffeeFormProps> = ( { onSubmit } ) => {
+  const [ name, setName ] = useState( "" );
+  const [ weight, setWeight ] = useState( 0 );
+  const [ price, setPrice ] = useState( 0 );
+  const [ roast, setRoast ] = useState( 0 );
+
+  const handleSubmit = ( e: React.FormEvent ) => {
+    e.preventDefault();
+    onSubmit( { name, weight, price, roast } );
+  };
+
+  const handleRoastChange = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+    const newRoast = parseInt( e.target.value );
+    if ( newRoast >= 1 && newRoast <= 5 ) {
+      setRoast( newRoast );
+    }
+  };
+
+  return (
+    <form onSubmit = {handleSubmit}>
+      <div className = "input-container">
+        <label htmlFor = "name">Name:</label>
+        <input
+          type = "text"
+          id = "name"
+          value = {name}
+          onChange = {( e ) => setName( e.target.value )}
+          required
+        />
+      </div>
+      <div className = "input-container-flex">
+        <div className = "input-container">
+          <label htmlFor = "weight">Weight:</label>
+          <input
+            type = "number"
+            id = "weight"
+            value = {weight}
+            onChange = {( e ) => setWeight( Number( e.target.value ) )}
+            required
+          />
+        </div>
+        <div className = "input-container">
+          <label htmlFor = "price">Price €:</label>
+          <input
+            type = "number"
+            id = "price "
+            value = {price}
+            onChange = {( e ) => setPrice( Number( e.target.value ) )}
+            required
+          />
+        </div>
+        <div className = "input-container">
+          <label htmlFor = "roast">Roast:</label>
+          <input
+            type = "number"
+            id = "roast"
+            value = {roast}
+            onChange = {handleRoastChange}
+            min = {1}
+            max = {5}
+          />
+        </div>
+      </div>
+
+      <button type = "submit">Add</button>
+    </form>
+  );
+};
+
+export default BeverageForm;

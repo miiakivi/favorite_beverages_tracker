100c23b2b156df250a9fb42229769f47987306ba
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-02-09 21:43:33 +0200

    refactor: added form and table components to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BeverageForm from "./components/BeverageForm";
+import BeverageTable from "./components/BeverageTable";
 
-function App() {
+import { BeverageType } from "./types/BeverageTypes";
+
+function App () {
+
+  const submitingNewBeverage = () => {
+    console.log( "new beverage" );
+  };
+
   return (
-    <div className="App">
-      <p>hello</p>
+    <div className = "App">
+      <div className = "wrapper">
+        <header>
+          <h1>My Favorite Beverages ☕️</h1>
+        </header>
+        <div className = "content-container">
+          <div className = "form-container">
+            <h2>Add beverage</h2>
+            <BeverageForm onSubmit = {submitingNewBeverage}></BeverageForm>
+          </div>
+
+          <BeverageTable beverageType = {BeverageType.Coffee}/>
+          <BeverageTable beverageType = {BeverageType.Tea}/>
+        </div>
+      </div>
+
     </div>
   );
 }
